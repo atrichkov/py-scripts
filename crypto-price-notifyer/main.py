26bf5c71a91df74aabd09b72c220c79f350eca38
Asen <asen.trichkov1@gmail.com>
2023-01-22 00:22:25 +0200

    price checker init

---
+++ crypto-price-notifyer/main.py
@@ -0,0 +1,24 @@
+import time
+import os
+import requests
+
+
+def message(title, content):
+    os.system('notify-send "' + title + '" "' + content + '"')
+
+
+# price = 0
+def check_price():
+    url = 'https://api.coingecko.com/api/v3/exchange_rates'
+    response = requests.get(url)
+    marketData = response.json()
+
+    current_price = round(marketData.get('rates').get('usd').get('value'), 0)
+    content = "BTC alert $ " + str(current_price)
+    message('Price alert', content)
+
+    return current_price
+
+while True:
+    time.sleep(3)
+    price = check_price()
